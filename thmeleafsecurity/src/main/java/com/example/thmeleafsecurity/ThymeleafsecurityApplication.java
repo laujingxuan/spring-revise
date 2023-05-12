b0a0625d26c655fb816e7cc24bf74ef504e00926
Jing Xuan Lau <jingxuan.lau@shopee.com>
2023-05-12 19:20:19 +0800

    thmyeleaf with Spring Security

---
+++ thmeleafsecurity/src/main/java/com/example/thmeleafsecurity/ThymeleafsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.thmeleafsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafsecurityApplication.class, args);
+	}
+
+}
---
+++ thmeleafsecurity/src/test/java/com/example/thmeleafsecurity/ThymeleafsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thmeleafsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
